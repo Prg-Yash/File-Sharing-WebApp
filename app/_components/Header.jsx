17771bed5ef36c3fd5e934e74ad6c6c07cdef3a0
Prg-Yash <127442680+Prg-Yash@users.noreply.github.com>
2024-07-19 19:16:31 +0530

    changes

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -36,14 +36,20 @@ export default function Header() {
       <div>
         <header className=" bg-gray-800 ">
           <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-            <Image src="/logo.svg" alt="logo" width={200} height={50} />
+            <Image
+              src="/logo.svg"
+              alt="logo"
+              width={150}
+              height={50}
+              className="my-2"
+            />
 
             <div className="flex flex-1 items-center justify-end md:justify-between">
               <nav aria-label="Global" className="hidden md:block">
                 <ul className="flex items-center gap-6 text-sm">
                   <li>
                     <a
-                      className="text-gray-500 transition hover:text-gray-500/75 text-white hover:text-white/75"
+                      className="transition  text-white hover:text-white/75"
                       href="/"
                     >
                       Home
@@ -52,7 +58,7 @@ export default function Header() {
 
                   <li>
                     <a
-                      className="text-gray-500 transition hover:text-gray-500/75 text-white hover:text-white/75"
+                      className=" transition  text-white hover:text-white/75"
                       href="/upload"
                     >
                       Upload
